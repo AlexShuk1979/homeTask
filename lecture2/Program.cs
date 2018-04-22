b603e899f7c56c2984a3d88d98f81a441e91df78
Алекс <@DESKTOP-F2JF73O>
2018-04-22 23:19:26 +0700

    massiv

--- lecture2/Program.cs
+++ lecture2/Program.cs
@@ -10,6 +10,20 @@ namespace lecture2
     {
         static void Main(string[] args)
         {
+            int n;
+            Console.WriteLine("введите размерность матрицы");
+            n = int.Parse(Console.ReadLine());
+            int[,] mat1 = new int[n, n];
+            int[,] mat2 = new int[n, n];
+            Random rnd = new Random();
+            for(int i=0; i<n;i++)
+                for(int j=0;j<n;j++)
+                {
+                    mat1[i, j] = rnd.Next(-100, 100);
+                    mat2[i, j] = rnd.Next(-100, 100);
+                }
+
+
         }
     }
 }
